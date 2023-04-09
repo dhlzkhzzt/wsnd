f90d61d7c733857f0d64e6b82e65ae20abcb7f38
dhlzkhzzt <130262112+dhlzkhzzt@users.noreply.github.com>
2023-04-09 02:05:38 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # set val
 python3 /app/self-ping.py &
 PORT=8080
-AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+AUUID=aae35ed5-de6f-4de4-8c4c-4c533e777677
 ParameterSSENCYPT=chacha20-ietf-poly1305
 CADDYIndexPage=https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
 
